0805c4d29039124b7130a112f0ca3b2d051f8fd4
il9ue <ilque.daniel@gmail.com>
2015-12-09 09:13:31 +0900

    problem asymTiling added, working on

--- DynamicProgrammingSets/AsymTiling.cpp
+++ DynamicProgrammingSets/AsymTiling.cpp
@@ -2,3 +2,35 @@
 /*
 	AsymTiling 
 */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class AsymTiling
+{
+public:
+	AsymTiling();
+	~AsymTiling();
+
+	int dp[101];
+	int MOD = 1000000007;
+
+	int tilingCount(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+
+		int &current = dp[n];
+		if (current) {
+			return current;
+		}
+
+		int nTiling = tilingCount(n - 1) + tilingCount(n - 2);
+		return nTiling % MOD;
+	}
+
+	
+};
